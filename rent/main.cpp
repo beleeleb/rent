8f061b2d54122396b60b4666131688b81d1b3440
gali zorea <galizorea@galis-macbook-pro-new.local>
2016-11-03 22:01:54 +0200

    Initial Commit

---
+++ rent/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rent
+//
+//  Created by gali zorea on 03/11/2016.
+//  Copyright © 2016 gali zorea. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
