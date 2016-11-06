ecde18b532989561936f8abbf8643cee3520ab12
gali zorea <galizorea@galis-macbook-pro-new.local>
2016-11-06 23:19:14 +0200

    my rent program first init commit

--- rent/main.cpp
+++ rent/main.cpp
@@ -7,9 +7,42 @@
 //
 
 #include <iostream>
+#include "db.h"
+#include "sched.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    db mydb;
+    sched mysched;
+    mydb.init();
+    cout << "products: " << mydb.getProdCount()<< endl;
+
+    mydb.newProd("bike",0, 2, 2); //name, prodid, rate, maxhour
+    mydb.newProd("book",1, 4, 2); //name, prodid, rate, maxhour
+    mydb.newProd("shoe",2, 5, 2); //name, prodid, rate, maxhour
+
+    mydb.newUser("yan1", mydb.userUnikey(), "1234");
+    mydb.newUser("yan2", mydb.userUnikey(), "4567");
+    mydb.newUser("yan3", mydb.userUnikey(), "5678");
+    mydb.newUser("yan4", mydb.userUnikey(), "6789");
+    cout << "users: " << mydb.getUserCount()<< endl;
+    
+    mydb.newRent(0, 1, 5); //prodid, useid, sval
+    mydb.newRent(0, 2, 3);
+    mydb.newRent(1, 1, 2);
+    mydb.newRent(2, 0, 1);
+    
+    cout << "products: " << mydb.getProdCount()<< endl;
+    cout << "rents: " << mydb.getRentsCount()<< endl;
+
+    mysched.init();
+    mysched.setTime(1, 1);
+    mysched.setTime(4,3);
+    mysched.setTime(6, 11);
+    
+    mysched.printSched();
     return 0;
 }
---
+++ rent/sched.h
@@ -0,0 +1,138 @@
+//
+//  sched.h
+//  rent
+//
+//  Created by gali zorea on 05/11/2016.
+//  Copyright © 2016 gali zorea. All rights reserved.
+//
+
+#ifndef sched_h
+#define sched_h
+
+class sched{
+public:
+    void init();
+    bool isone(int _index);
+    bool setTime(int _day, int _hour);
+    void printSched();
+    unsigned long printSval();
+private:
+    unsigned long m_schedBitArr[2];
+};
+
+
+void sched::init()
+{
+    memset(&m_schedBitArr, 0, sizeof(m_schedBitArr));
+}
+
+
+bool sched::setTime(int _day, int _hour)//day 1-7, hour 1-12
+{
+    int index = 0;
+    _day -= 1;  //for UI purposes only
+    _hour -= 1;
+    index = _day * 12 + _hour;
+    if(index <= 83)
+    {
+        if(index >= 64)
+        {
+            m_schedBitArr[1] |= 1UL << (index - 64);    //UL stands for (unsigned long) because 1 is int by default
+        }
+        
+        else
+        {
+            m_schedBitArr[0] |= 1UL << index;
+        }
+        
+        return true; //set succeeded
+    }
+    
+    else
+    {
+        return false; //day and hour more than 84 (7 * 12)
+    }
+}
+
+void sched::printSched()
+{
+    int i_day = 0;
+    int j_hour = 0;
+    int index = 0;
+    
+    cout << "\t\t";
+    
+    for(j_hour = 1; j_hour < 10; j_hour++)
+    {
+        cout << "-" << j_hour << "-";
+    }
+    
+    for(; j_hour < 13; j_hour++)
+    {
+        cout  << j_hour << "-";
+    }
+    cout << endl;
+
+    j_hour = 0;
+    for (i_day = 0; i_day < 7; i_day++)
+    {
+        index = i_day * 12;
+        
+        cout << "day " << i_day + 1 << ":\t";
+        for(j_hour = 0; j_hour < 12; j_hour++)
+        {
+            index += j_hour;
+            
+            if(index >= 64)
+            {
+                if(m_schedBitArr[1] & 1UL << (index - 64))
+                {
+                    cout << "|*|" ;
+                }
+                
+                else{
+                    cout << "| |" ;
+                }
+            }
+            
+            else
+            {
+                if(m_schedBitArr[0] & (1UL << index))
+                {
+                    cout << "|*|" ;
+                }
+            
+                else
+                {
+                    cout << "| |" ;
+                }
+            }
+            
+            index = i_day * 12;
+
+        }
+        
+        cout << endl;
+
+        
+        
+    }
+}
+bool sched::isone(int _index)
+{
+  /*  if(_index > 127)
+    {
+        if(_index > 63)
+        {
+            
+        }
+        
+        else{
+            if()
+        }
+    }
+*/
+    return true;
+}
+
+#endif /* sched_h */
